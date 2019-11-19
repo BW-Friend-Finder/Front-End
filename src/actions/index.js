88321bbb089b37fe73e7a87b923749d19e0ead84
Michael Jordan <Insurikai@gmail.com>
2019-11-18 17:21:45 -0800

    Actions for user managment

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,30 @@ Dislike [(other user's id and current user's id)]
 ~Recieve Message (userid, conversation id)
 Update Profile - send back user object (optionals - just changes)
 */
+export const LOGIN;
+export const REGISTER;
+export const LIKE;
+export const DISLIKE;
+export const SEND_MESSAGE;
+export const RECEIVE_MESSAGES;
+export const UPDATE_PROFILE;
+
+export const login = loginInfo => dispatch => {
+  dispatch({
+    type: LOGIN,
+    payload: loginInfo
+  });
+}
+export const register = registerInfo => dispatch => {
+  dispatch({
+    type: REGISTER,
+    payload: registerInfo
+  })
+  dispatch({
+    type: LOGIN,
+    payload: {
+      email: registerInfo.email,
+      password: registerInfo.password
+    }
+  });
+}
