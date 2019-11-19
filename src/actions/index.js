f7a55a0e43e70db7b78303522e9f7e7529318b05
Michael Jordan <Insurikai@gmail.com>
2019-11-18 17:19:24 -0800

    outline for actions

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+/*
+Login
+Register
+Like [(other user's id and current user's id)]
+Dislike [(other user's id and current user's id)]
+~Send Message (userid, conversation id, message)
+~Recieve Message (userid, conversation id)
+Update Profile - send back user object (optionals - just changes)
+*/
